6ef7270051d5e1fc3e4906ea8c53fa6872cef1a0
gnomedevreact <143091438+gnomedevreact@users.noreply.github.com>
2024-02-20 22:24:01 +0300

    patch:Enabled

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -1,11 +1,16 @@
+import { usePathname } from "next/navigation";
+
 import { UsersService } from "@/services/users.service";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useChat = () => {
+  const pathname = usePathname();
+
   const { data: rooms, isLoading } = useQuery({
     queryKey: ["get rooms"],
     queryFn: () => UsersService.getUserRooms(),
     select: ({ data }) => data,
+    enabled: pathname === "/",
   });
 
   return { rooms, isLoading };
