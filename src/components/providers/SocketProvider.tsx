03d10e93741ed575778db9f2e61e02895af3f759
gnomedevreact <143091438+gnomedevreact@users.noreply.github.com>
2024-02-20 21:48:18 +0300

    patch:WssHost

--- src/components/providers/SocketProvider.tsx
+++ src/components/providers/SocketProvider.tsx
@@ -32,7 +32,7 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
       setRooms(rooms);
       const roomIds = rooms.map((room) => room.id);
 
-      const newSocket = io("wss://next-new-nu.vercel.app");
+      const newSocket = io("wss://nest-new.onrender.com");
 
       newSocket.on("connect", () => {
         console.log("connected");
